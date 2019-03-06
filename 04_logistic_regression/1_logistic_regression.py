13b20f574eb28b9a628f3ca31eb4af2de723ee39
jh9999995 <942642428@qq.com>
2019-03-06 15:30:06 +0800

    logistic regression updated

--- 04_logistic_regression/1_logistic_regression.py
+++ 04_logistic_regression/1_logistic_regression.py
@@ -4,10 +4,11 @@
  @author: Jiang Hui
  @language:Python 3.7.2 [GCC 7.3.0] :: Anaconda, Inc. on linux
  @time: 3/2/19 2:05 PM
- @desc:
+ @desc: 了解梯度下降的算法原理，掌握传统计算方法和向量化计算方法，最终通过代码实现
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 """
  引子:
@@ -38,7 +39,7 @@ import matplotlib.pyplot as plt
         首先，机器学习的损失函数是人为设计的，用于评判模型好坏（对未知的预测能力）的一个标准、尺子，就像去评判任何一件事物一样，从不同角度
     看往往存在不同的评判标准，不同的标准往往各有优劣，并不冲突。唯一需要注意的就是最好选一个容易测量的标准，不然就难以评判了。
         其次，既然不同标准并不冲突，那使用最小二乘法作为逻辑回归的损失函数当然是可以，那这里为什么不用最小二乘而用最大似然呢？
-    请看一下最小二乘作为损失函数的函数曲线(least_square_error.jpg)以及最大似然作为损失函数的函数曲线(max_likelihood_estimation.jpg),
+    请看一下最小二乘作为损失函数的函数曲线(figure_non_convex.jpg)以及最大似然作为损失函数的函数曲线(figure_convex.jpg),
     显然发现最大似然作为损失函数，图像为凸函数，容易得到参数的最优解，而最小二乘作为损失函数时，图像非凸，容易陷入到局部最优。
  
     (1)我们前面提到，h(X)可理解为样本X属于类别1的概率，因此：
@@ -48,7 +49,7 @@ import matplotlib.pyplot as plt
                 P(y|X,θ) = h(X)^y * (1 - h(X))^(1-y)  ,  其中y的取值为0或1
         得到了y的概率分布函数表达式，我们可以用似然函数最大化来求解我们需要的模型参数θ !!!
 
-【重点】    
+   【重点】    
     (2)解释一下，为什么用最大似然估计来求解参数θ时要用到累乘？
        我们假设训练样本之间是独立同分布的，我们现在想做的，就是根据已有的样本特征矩阵X，和类别向量Y，来找到一组合适的参数集θ，
     使得每一个样本与θ组合再经过函数变换之后，得到的值与其真实值尽可能一致，也就是说，这个参数向量θ，要满足下式：
@@ -69,10 +70,15 @@ import matplotlib.pyplot as plt
         J(θ) = -sum( yi*(ln h(xi)) + (1-yi)*ln(1-h(xi)) ,其中i表示第i个样本
         
  3.二元逻辑回归的损失函数的最优化问题
-    对于二元逻辑回归的损失函数极小化，有比较多的方法，最常见的有梯度下降法，坐标轴下降法，牛顿法等。本次学习中，我用的方法是梯度下降法，我在
- 本目录下，写了一个gradient_descent_demo.py代码，以一个函数实例，详细介绍了用梯度下降法求极值的思路与代码实现过程。
-    了解完梯度下降之后，我们需要关心的就是每次迭代时θ的更新公式，关于这个，我手推了θ的更新过程，在本目录gradient_descent_compute.jpg中，
- 至此，我们正式开始用Python实现逻辑回归的代码。
+    对于二元逻辑回归的损失函数极小化，有比较多的方法，最常见的有梯度下降法，坐标轴下降法，牛顿法等。
+    本次学习中我用的方法是梯度下降法，我在本目录下，写了一个gradient_descent_demo.py代码，以一个函数实例，
+ 详细介绍了用梯度下降法求极值的思路与代码实现过程。
+    (1)了解完梯度下降之后，我们需要关心的就是每次迭代时θ的更新公式，关于这个，我手推了θ的更新过程，实现代码为gradient_descent；
+    (2)书上的实现方式是通过向量的方式进行计算，降低了计算的复杂度，实现代码为gradient_descent_matrix
+    (3)上面的方式，在每次更新回归系数时，都需要遍历整个数据集，计算复杂度过大，改进方法是随机梯度下降，
+       一次仅用一个样本点来更新回归系数，可以在新样本到来时对分类器进行增量式更新，是一种在线学习方法，而一次性处理所有数据，被称作是“批处理”
+       实现代码为stochastic_gradient_descent()
+
 """
 
 
@@ -91,18 +97,14 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 
-"""
- 这是用到的第一种方法，按照我推导的公式(gradient_element_compute.jpg)来编写的代码，算法如下:
-    (1)初始化参数向量theta为一个元素值全为1,形状为(样本特征数, 1)的列向量，不一定非得是1，全为0也行；
-    (2)开始迭代，迭代停止条件为达到迭代次数
-        (a) 遍历向量theta的每一个元素，执行下面操作：
-            θ_j = θ_j + α * sum[(y_i - sigmoid(θ.T * x_i))*x_i_j]
-            其中j表示θ的第j个元素，y_i表示第i个样本的类别，x_i表示第i个样本的特征向量，x_i_j为x_i的第j个元素
-        (b) 迭代次数减1
-    (3) 返回参数向量θ
-"""
-
-
+#  这是用到的第一种方法，按照我推导的公式(manuscript_1.jpg)来编写的代码，算法如下:
+#     (1)初始化参数向量theta为一个元素值全为1,形状为(样本特征数, 1)的列向量，不一定非得是1，全为0也行；
+#     (2)开始迭代，迭代停止条件为达到迭代次数
+#         (a) 遍历向量theta的每一个元素，执行下面操作：
+#             θ_j = θ_j + α * sum[(y_i - sigmoid(θ.T * x_i))*x_i_j]
+#             其中j表示θ的第j个元素，y_i表示第i个样本的类别，x_i表示第i个样本的特征向量，x_i_j为x_i的第j个元素
+#         (b) 迭代次数减1
+#     (3) 返回参数向量θ
 def gradient_descent(data_matrix, label_vector, alpha, num_of_rounds):
     theta_vector = np.ones((np.shape(data_matrix)[1], 1))  # 初始化θ 为一个长度与特征矩阵的列数相同的元素值全为1的列向量
     while num_of_rounds:
@@ -116,22 +118,17 @@ def gradient_descent(data_matrix, label_vector, alpha, num_of_rounds):
     return theta_vector
 
 
-"""
- 这是第二种梯度下降的方法，按照推导式(gradient_vector_compute.jpg)直接对整个θ向量更新，算法如下：
-    (1)样本特征转为(样本数，特征数)的矩阵X
-    (2)样本类别转为(样本数，1)的矩阵Y
-    (3)初始化一个元素值全为1，形状为(特征数，1)的向量θ
-    (4)开始迭代：
-        (a)计算梯度向量，公式为:
-            gradient_vec = X.T * (sigmoid(Xθ)-Y)
-        (b)更新向量θ，公式为:
-            θ = θ - α*gradient_vec
-        (c)迭代次数减1
-    (5) 返回θ
-    
-"""
-
-
+# 这是第二种梯度下降的方法，按照推导式(manuscript_2.jpg)直接对整个θ向量更新，算法如下：
+#    (1)样本特征转为(样本数，特征数)的矩阵X
+#    (2)样本类别转为(样本数，1)的矩阵Y
+#    (3)初始化一个元素值全为1，形状为(特征数，1)的向量θ
+#    (4)开始迭代：
+#        (a)计算梯度向量，公式为:
+#            gradient_vec = X.T * (sigmoid(Xθ)-Y)
+#        (b)更新向量θ，公式为:
+#            θ = θ - α*gradient_vec
+#        (c)迭代次数减1
+#    (5) 返回θ
 def gradient_descent_matrix(data_matrix, label_vector, alpha, num_of_rounds):
     data_matrix = np.mat(data_matrix)  # 转换为(100,3)的矩阵，每一行代表一个样本的特征向量
     label_vector = np.mat(label_vector).transpose()  # 转换为(100,1)的列向量
@@ -139,6 +136,30 @@ def gradient_descent_matrix(data_matrix, label_vector, alpha, num_of_rounds):
     while num_of_rounds:
         gradient_vector = data_matrix.T * (sigmoid(data_matrix * theta_vector) - label_vector)
         theta_vector = theta_vector - alpha * gradient_vector
+        num_of_rounds -= 1
+    return theta_vector
+
+
+# 随机梯度下降算法,按照我推导的公式(manuscript_3.jpg)来编写的代码，算法如下伪代码：
+#    (1)参数向量θ初始化为元素值全为1,长度为特征数的列向量
+#    (2)开始迭代:
+#        对数据集中的每一个样本
+#            计算梯度gradient = (sigmoid(X[i]*θ)-yi) * (X[i].T) , 数值 * 列向量的形式
+#            使用alpha*gradient来更新值
+#        迭代次数减1
+#    (3)迭代完成，返回参数向量
+def stochastic_gradient_descent(feature_mat, label_vec, alpha, num_of_rounds):
+    num_of_samples = len(feature_mat)
+    num_of_features = len(feature_mat[0])
+    feature_mat = np.mat(feature_mat)
+    label_vec = np.mat(label_vec).transpose()  # 转置成(100,1)的列向量
+    theta_vector = np.ones((num_of_features, 1))
+    while num_of_rounds:
+        for i in range(num_of_samples):
+            # error是某个样本的特征值与参数θ点乘之后，经过sigmoid函数转换后的结果与真实值yi的误差,形如[[e]]
+            error = sigmoid(feature_mat[i] * theta_vector) - label_vec[i]
+            gradient = error[0, 0] * feature_mat[i].T
+            theta_vector = theta_vector - alpha * gradient
         num_of_rounds -= 1
     return theta_vector
 
@@ -153,7 +174,7 @@ def gradient_descent_matrix(data_matrix, label_vector, alpha, num_of_rounds):
 def plot_best_fit(weight_vec):
     mat, vec = load_data_set()  # 加载数据集
     data_arr = np.array(mat)  # 把list类型的mat,转换成ndArray类型
-    n = np.shape(features)[0]  # 数据的个数
+    n = np.shape(mat)[0]  # 数据的个数
     x_1 = []  # 类别为1的样本的横坐标
     y_1 = []  # 类别为1的样本的纵坐标
     x_0 = []  # 类别为0的样本的横坐标
@@ -181,11 +202,33 @@ def plot_best_fit(weight_vec):
 
 if __name__ == '__main__':
     features, labels = load_data_set()
-    # 可通过调整alpha与num_of_rounds的参数组合，来找到最佳拟合线，通常来说，以alpha*num_of_rounds乘积不变的情况下进行调整
-    # weights = gradient_descent(features, labels, alpha=0.001, num_of_rounds=2000)
-    weights = gradient_descent_matrix(features, labels, alpha=0.001, num_of_rounds=2000)
-    weights = np.array(weights)  # 第二种方法生成的weights是一个矩阵的类型，这里为了绘图方便，转成array向量的形式
+    # 这里我想对比一下，不同算法程序的运行时间
+    start_time = datetime.datetime.now()
+    weights = gradient_descent(features, labels, alpha=0.001, num_of_rounds=2000)
+    # weights = gradient_descent_matrix(features, labels, alpha=0.001, num_of_rounds=2000)
+    # weights = stochastic_gradient_descent(features, labels, alpha=0.001, num_of_rounds=2000)
+    end_time = datetime.datetime.now()
+    running_time = (end_time - start_time).microseconds
+    print("程序运行时长为：{}s".format(running_time))
     print(weights)
+    weights = np.array(weights)  # weights 转成array向量的形式
     plot_best_fit(weights)
 
-# 对比两次不同梯度下降方法的实验结果，发现结果是一致的，但是第二种方法直接用矩阵运算，相对于1来说，少了很多for循环的过程，速度明显快得多
+"""
+ 对比一下三种实现方式的结果, 基于统一数据集，同样的学习步长，同样的迭代次数
+ |--------------------------------------------------------------------------------------------------|
+ |         算法                             运行时长                         参数值                    |
+ |  gradient_descent                         3.6s             [7.01092219, 0.70376226, -0.98513334] |   
+ |                                                                                                  |
+ |  gradient_descent_matrix                 0.086s            [6.99233391, 0.70237023, -0.98258342] |                              
+ |                                                                                                  |
+ |  stochastic_gradient_descent              5.3s             [7.00009837, 0.70071137, -0.98870703] | 
+ |--------------------------------------------------------------------------------------------------|
+ 
+ 对应的三张分类结果图分别保存为model_fit_line_1.jpg, model_fit_line_2.jpg, model_fit_line_3.jpg
+ 总结：
+    观察发现，在本案例中，三种方式得出的结果差异不大，但是最快的方式，显然是直接通过矩阵的方式，来计算最终的回归系数，
+ 最慢的是随机梯度下降法，这说明：
+    在样本量较少且样本维度较低时，建议使用批处理梯度下降(矩阵形式)；
+    在数据量庞大或者样本维度很高时，建议采用随机梯度下降；（在使用随机梯度下降时，可以对数据集进行shuffle处理，使样本具有随机性）
+"""
