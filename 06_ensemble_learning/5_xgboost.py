cc95959d2bdfead29611eb7cc931ac962c5a6d23
jh9999995 <942642428@qq.com>
2019-03-26 16:17:04 +0800

    ensemble updated

--- 06_ensemble_learning/5_xgboost.py
+++ 06_ensemble_learning/5_xgboost.py
@@ -11,6 +11,7 @@
  引言：
     XGBOOST，全称是 extreme gradient boosting ，它的演化过程如下：
         
-    Tree -> Boosting Tree -> Gradient Boosting Tree -> Extreme Gradient Boosting Tree
+    Tree -> Boosting Tree -> Gradient Boosting Tree -> Extreme Gradient Boosting Tree  
+    (注：xgboost的基学习器除了CART回归树还支持线性分类器，这个时候的xgboost相当于L1和L2正则化的LR(分类)或者LinearRegression(回归))
                                     
 """
